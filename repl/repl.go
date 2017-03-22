2b914ffd8269dcc91e06662555d75fd0fca9f8fb
James Routley <jroutley@gmail.com>
2017-03-22 09:28:38 +0000

    Add repl

---
+++ repl/repl.go
@@ -0,0 +1,33 @@
+package repl
+
+import(
+  "bufio"
+  "fmt"
+  "io"
+  "github.com/jamesroutley/monkey/lexer"
+  "github.com/jamesroutley/monkey/token"
+)
+
+// PROMPT is the prompt string to print at the repl.
+const PROMPT = ">> "
+
+// Start starts the Monkey repl.
+func Start(in io.Reader, out io.Writer) {
+  scanner := bufio.NewScanner(in)
+
+  for {
+    fmt.Printf(PROMPT)
+    scanned := scanner.Scan()
+    if !scanned {
+      return
+    }
+
+    line := scanner.Text()
+    l := lexer.New(line)
+
+    for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+      fmt.Printf("%+v\n", tok)
+    }
+
+  }
+}
