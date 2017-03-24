2bbf8daa2abc6fa18f4ddfa8d94aa311bf5eb1c7
James Routley <jroutley@gmail.com>
2017-03-24 19:17:54 +0000

    Add parsing for let statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,53 +1,73 @@
+// Package ast implements Monkey's Abstract Syntax Tree
 package ast
 
 import (
-  "github.com/jamesroutley/monkey/token"
+	"github.com/jamesroutley/monkey/token"
 )
 
+// Node is an item in the AST.
 type Node interface {
-  TokenLiteral() string
+	TokenLiteral() string
 }
 
+// Statement represents a Monkey statement in the AST.
+// Statements are language constructs which don't produce values. Let, if and
+// return are all statements.
 type Statement interface {
-  Node
-  statementNode()
+	Node
+	// Dummy method used to make the Go compiler distinguish Statements from
+	// Expressions.
+	statementNode()
 }
 
+// Expression represents a Monkey expression in the AST.
+// Expressions are language constructs which produce values. Number or string
+// literals and mathematical equations examples of expressions.
 type Expression interface {
-  Node
-  expressionNode()
+	Node
+	// Dummy method used to make the Go compiler distinguish Expressions
+	// from Statements.
+	expressionNode()
 }
 
+// Program is the root of the AST and represents the program itself.
 type Program struct {
-  Statements []Statement
+	Statements []Statement
 }
 
 func (p *Program) TokenLiteral() string {
-  if len(p.Statements) > 0 {
-    return p.Statements[0].TokenLiteral()
-  }
-  return ""
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
 }
 
+// LetStatement represents a variable binding statement in the AST.
+// Let statements are denoted by the keyword 'let'.
 type LetStatement struct {
-  Token token.Token
-  Name *Identifier
-  Value Expression
+	Token token.Token
+	Name  *Identifier
+	Value Expression
 }
 
 func (ls *LetStatement) statementNode() {}
 
+// TokenLiteral returns the literal value of the token associated with the
+// statement
 func (ls *LetStatement) TokenLiteral() string {
-  return ls.Token.Literal
+	return ls.Token.Literal
 }
 
+// Identifier represents a variable name in the AST.
 type Identifier struct {
-  Token token.Token
-  Value string
+	Token token.Token
+	Value string
 }
 
 func (i Identifier) expressionNode() {}
 
+// TokenLiteral returns the literal value of the token associated with the
+// identifier
 func (i Identifier) TokenLiteral() string {
-  return i.Token.Literal
+	return i.Token.Literal
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,33 +1,100 @@
+// Package parser implements Monkey's parser.
 package parser
 
 import (
-  "github.com/jamesroutley/monkey/ast"
-  "github.com/jamesroutley/monkey/lexer"
-  "github.com/jamesroutley/monkey/token"
+	"github.com/jamesroutley/monkey/ast"
+	"github.com/jamesroutley/monkey/lexer"
+	"github.com/jamesroutley/monkey/token"
 )
 
+// Parser implements the parser for the Monkey language.
 type Parser struct {
-  l *lexer.Lexer
+	l *lexer.Lexer // Lexer used to lex the Money code.
 
-  curToken token.Token
-  peekToken token.Token
+	curToken  token.Token // Current token being parsed.
+	peekToken token.Token // Next token to be parsed.
 }
 
+// New initialises and returns a pointer to a Parser.
 func New(l *lexer.Lexer) *Parser {
-  p := &Parser{l: l}
+	p := &Parser{l: l}
 
-  // Read two tokens, so curToken and peekToken are both set
-  p.nextToken()
-  p.nextToken()
+	// Read two tokens, so curToken and peekToken are both set.
+	p.nextToken()
+	p.nextToken()
 
-  return p
+	return p
 }
 
+// nextToken increments the current and peek tokens.
 func (p *Parser) nextToken() {
-  p.curToken = p.peekToken
-  p.peekToken = p.l.NextToken()
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
 }
 
+// ParseProgram parses the program loaded into the lexer and returns its AST.
+// Monkey programs consist of a list of statements. These statements are
+// recursively parsed, one by one.
 func (p *Parser) ParseProgram() *ast.Program {
-  return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+// parseStatement parses any Monkey statement.
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+// parseLetStatement parses 'let' statements.
+// e.g. 'let x = 5;'
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	return false
 }
