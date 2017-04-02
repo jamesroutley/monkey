948f27fa48d2062d1212e80a6acc6baa4e6aeb56
James Routley <jroutley@gmail.com>
2017-04-02 10:27:32 +0100

    Add return statement parsing

--- ast/ast.go
+++ ast/ast.go
@@ -58,6 +58,19 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+// ReturnStatement represents a return statement in the AST.
+type ReturnStatement struct {
+	Token token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+// TokenLiteral returns the literal value of the return token.
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
 // Identifier represents a variable name in the AST.
 type Identifier struct {
 	Token token.Token
