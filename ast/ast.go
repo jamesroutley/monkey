851bd8c98122cec03dc804bf91d137ca6a85f72d
James Routley <jroutley@gmail.com>
2017-04-13 11:37:15 +0200

    Add integer literal parsing

--- ast/ast.go
+++ ast/ast.go
@@ -140,3 +140,18 @@ func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
