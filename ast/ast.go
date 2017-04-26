74d2657f304f9d2fcd6c712920dd2b4361287236
James Routley <jroutley@gmail.com>
2017-04-26 10:53:32 +0100

    Add function literal parsing

--- ast/ast.go
+++ ast/ast.go
@@ -3,8 +3,8 @@ package ast
 
 import (
 	"bytes"
-
 	"github.com/jamesroutley/monkey/token"
+	"strings"
 )
 
 // Node is an item in the AST.
@@ -259,5 +259,31 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.Token.Literal)
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
+
 	return out.String()
 }
